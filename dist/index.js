6595cb1f1f2fd3ea87b85bfec1b297fc3b1b491a
igitonga <iangitonga47@gmail.com>
2024-06-06 09:28:54 +0300

    Fix for reload

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,16 @@
-import { twitterClient } from './twitterClient.js';
-import { runPrompt } from './gemini.js';
-// const app: Application = express();
-// const port: number = 3000;
-const tweet = async () => {
-    const content = await runPrompt();
-    console.log(content);
-    try {
-        await twitterClient.v2.tweet(content);
-    }
-    catch (error) {
-        console.log(error);
-    }
-};
-tweet();
-// app.listen(port, () => {
-//     console.log(`Server listening on port ${port}`);
-// })
+import express from 'express';
+const app = express();
+const port = 3000;
+// const tweet = async () => {
+//     const content = await runPrompt();
+//     console.log(content);
+//     try {
+//         await twitterClient.v2.tweet(content);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// tweet();
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,21 @@ import express, {Application, Response, Request} from 'express';
 import { twitterClient } from './twitterClient.js';
 import { runPrompt } from './gemini.js';
 
-// const app: Application = express();
-// const port: number = 3000;
+const app: Application = express();
+const port: number = 3000;
 
-const tweet = async () => {
-    const content = await runPrompt();
-    console.log(content);
-    try {
-        await twitterClient.v2.tweet(content);
-    } catch (error) {
-        console.log(error)
-    }
-}
+// const tweet = async () => {
+//     const content = await runPrompt();
+//     console.log(content);
+//     try {
+//         await twitterClient.v2.tweet(content);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
-tweet();
+// tweet();
 
-// app.listen(port, () => {
-//     console.log(`Server listening on port ${port}`);
-// })
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
