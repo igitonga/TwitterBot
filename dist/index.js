741fb9b4041c215a127bc462614743938db1dd4f
igitonga <iangitonga47@gmail.com>
2024-06-06 09:57:53 +0300

    Fix for reload

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,23 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { twitterClient } from './twitterClient.js';
+import { runPrompt } from './gemini.js';
+dotenv.config();
 const app = express();
-const port = 3000;
-// const tweet = async () => {
-//     const content = await runPrompt();
-//     console.log(content);
-//     try {
-//         await twitterClient.v2.tweet(content);
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-// tweet();
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+app.get('/', (req, res) => {
+    res.send('This is Twitter Bot!');
+});
+const tweet = async () => {
+    const content = await runPrompt();
+    console.log(content);
+    try {
+        await twitterClient.v2.tweet(content);
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+//tweet();
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
 import express, {Application, Response, Request} from 'express';
+import dotenv from "dotenv";
 import { twitterClient } from './twitterClient.js';
 import { runPrompt } from './gemini.js';
 
+dotenv.config();
+
 const app: Application = express();
-const port: number = 3000;
 
-// const tweet = async () => {
-//     const content = await runPrompt();
-//     console.log(content);
-//     try {
-//         await twitterClient.v2.tweet(content);
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+app.get('/', (req: Request, res: Response) => {
+    res.send('This is Twitter Bot!')
+})
 
-// tweet();
+const tweet = async () => {
+    const content = await runPrompt();
+    console.log(content);
+    try {
+        await twitterClient.v2.tweet(content);
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+//tweet();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`);
 })
